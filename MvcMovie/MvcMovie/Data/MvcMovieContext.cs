c1dc2e98ed8b06a10050c3985ca60be932294138
Bohdan Filimonyuk <bogdan1999kovel@gmail.com>
2019-02-28 08:52:17 +0100

    Init(Identity imported)

---
+++ MvcMovie/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    //public class MvcMovieContext : DbContext
+    //{
+    //    public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+    //        : base(options)
+    //    {
+    //    }
+    public class MvcMovieContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        { }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+
+
+}
---
+++ MvcMovie/MvcMovie/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+//using Microsoft.AspNetCore.Session;
+
+namespace MvcMovie
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseNpgsql(Configuration.GetConnectionString("Default")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+            {
+                options.Cookies.ApplicationCookie.AccessDeniedPath = "/accessdenied";
+                options.Cookies.ApplicationCookie.LoginPath = "/login";
+                options.Cookies.ApplicationCookie.LoginPath = "/logout";
+            })
+        .AddEntityFrameworkStores<MvcMovieContext/*, Guid*/>()
+        .AddDefaultTokenProviders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
